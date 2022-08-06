49dcb522c3e67da727fdcf3905d3ff4a59d47196
Mikhail <mvnorkin@gmail.com>
2022-08-06 21:38:49 +0300

    task83 is Complite

--- Task83/Program.cs
+++ Task83/Program.cs
@@ -7,3 +7,35 @@
 18 20
 15 18
 */
+
+//int[,] array1 = new int[m,n];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            array[i,j] = new Random().Next(6);
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array1 = new int[2,2];
+int[,] array2 = new int[2,2];
+FillArray(array1);
+Console.WriteLine();
+FillArray(array2);
+Console.WriteLine();
+
+int[,] array = new int[2,2];
+for (int i = 0; i < 2; i++)
+{
+    for(int j = 0; j < 2; j++)
+    {
+        array[i,j] = array1[i,j]*array2[i,j];
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
