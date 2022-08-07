ecfa2945a030faadd2d54cd49edb107205dd6ca5
Mikhail <mvnorkin@gmail.com>
2022-08-07 22:03:04 +0300

    task85 is complite

--- Task85/Program.cs
+++ Task85/Program.cs
@@ -7,8 +7,6 @@
 10 09 08 07
 */
 
-
-
 void FillArray()
 {
     int n = 1, m = 0;
@@ -19,33 +17,36 @@ void FillArray()
     {
         for (int j = 0;;)
         {   
-            if (m == 15)
+            if (m == 16)
             {
                 for (int ii = 0; ii < 4; ii++)
                     {
                         for(int jj = 0; jj < 4; jj++)
-                        {
-                            Console.Write($"i = {ii} j = {jj} {array[ii,jj]}    ");
+                        {   
+                            if (array[ii,jj]<10)
+                            {
+                                Console.Write($"0{array[ii,jj]} ");     
+                            }
+                            else
+                            {
+                                Console.Write($"{array[ii,jj]} ");
+                            }
                         }
                         Console.WriteLine();
                     }
                 return;    
             }
 
-
             m = m + 1;
             array[i,j] = m;
-            Console.WriteLine($"i= {i} ");
-            Console.WriteLine($"j= {j} ");
-            Console.WriteLine($"{array[i,j]} ");
+
             i+=a;
             j+=b;            
 
-            //Console.WriteLine($"{j} ");
             if      (j==4 )  {i=1; j=3; n = 2;}
             else if (i==4 )  {i=3; j=2; n = 3;}
             else if (j==-1)  {i=2; j=0; n = 4;}
-            if (m>11 )
+            if (m>10 )
                 {
                     if (n==4)
                     {
@@ -55,15 +56,15 @@ void FillArray()
                     }
                     else if (n==1)
                     {
-                        if (array[i+1,j] != 0) {n=2;}
+                        if (array[i,j+1] != 0) {n=2;}
                     }
                     else if (n==2)
                     {
-                        if (array[i,j+1] != 0) {n=3;}
+                        if (array[i+1,j] != 0) {n=3;}
                     }
                     else if (n==3)
                     {
-                        if (array[i-1,j] != 0) {n=4;}
+                        if (array[i,j-1] != 0) {n=4;}
                     }
                 }
 
